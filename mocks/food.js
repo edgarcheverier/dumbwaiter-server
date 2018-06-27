95b77ec75dfc67c438fc08fd608b81f7745d452c
BenjaKemp <benjokemp@gmail.com>
2018-06-27 09:50:13 +0200

    pictures added (#8)

--- mocks/food.js
+++ mocks/food.js
@@ -3,7 +3,7 @@ module.exports = [
     name: "Soup of the Day (Check Specials Board)",
     description: "",
     price: "5.95",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0029_chicken%20soup.jpg?alt=media&token=ef8ddf80-1867-4352-88f8-80d183ebc39d",
     categories: ['foods', 'starters']
   },
   {
@@ -17,222 +17,204 @@ module.exports = [
     name: "Chicken Wings",
     description: "(Contains MK, CY) Hot ‘n’ Spicy wings, celery spear, blue cheese & garlic dip",
     price: "8.95 ",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0028_chicken%20wings.jpg?alt=media&token=d01c481a-217b-42c2-b593-9492e3540689",
     categories: ['foods', 'starters']
   },
   {
     name: "Baby Ribs",
     description: "(Contains MK, S, SP) Slow cooked pork ribs glazed with BBQ sauce and served with chargrilled corn on the cob",
     price: "9.95",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0027_ribs.jpg?alt=media&token=8c7d8b60-143d-4a89-b680-8a177532eb30",
     categories: ['foods', 'starters']
   },
   {
     name: "Feta Cheese Salad",
     description: "(Contains MK, N) Dressed leaves, Cherry tomatoes, Beetroot & Walnuts topped with Feta Cheese",
     price: "6.95",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0026_feta.jpg?alt=media&token=755354c7-ce23-41d5-8c72-2c4c4530d01a",
     categories: ['foods', 'starters']
   },
   {
     name: "Prawn Pil Pil",
     description: "(Contains MK, G, C) Sauté Dublin bay prawns in a hot chilli & garlic oil, sourdough bread",
     price: "12.95",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0025_prawns.jpg?alt=media&token=1829c021-dfc6-4391-ac69-872e83e1ce52",
     categories: ['foods', 'starters']
   },
   {
     name: "Black Pudding Salad",
-    description:
-      "(Contains G, E) Tom’s finest black pudding tossed on the pan with bacon, sauté potato & topped with poached egg, tossed leaves & balsamic glaze",
+    description: "(Contains G, E) Tom’s finest black pudding tossed on the pan with bacon, sauté potato & topped with poached egg, tossed leaves & balsamic glaze",
     price: "8.95",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0024_black%20pudding%20salad.jpg?alt=media&token=e4116067-fe46-4baf-9a62-0c2c1b823780",
     categories: ['foods', 'starters']
   },
   {
     name: "Nachos",
-    description:
-      "(Contains G, MK) Chilli beef nachos, tortilla chips, sour cream & salsa OR Veggies with mix courgette, peppers & onion in a tomato sauce with melting cheese",
+    description: "(Contains G, MK) Chilli beef nachos, tortilla chips, sour cream & salsa OR Veggies with mix courgette, peppers & onion in a tomato sauce with melting cheese",
     price: "8.95",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0020_nachos.jpg?alt=media&token=cc6b278f-fe63-49aa-b53a-f17a29936ad9",
     categories: ['foods', 'starters']
   },
   {
     name: "Fish Cakes",
-    description:
-      "(Contains G, MK, C, E, SS, N) Fresh cod, salmon & crab meat in a sesame crumb, chilli, coriander & lime mayonnaise, Rocket & pesto dressing",
+    description:  "(Contains G, MK, C, E, SS, N) Fresh cod, salmon & crab meat in a sesame crumb, chilli, coriander & lime mayonnaise, Rocket & pesto dressing",
     price: "8.95",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0023_fish%20cakes.jpg?alt=media&token=171fc2ec-cd28-4002-93d5-bb1b04e88622",
     categories: ['foods', 'starters']
   },
   {
     name: "Stocky's Sharing Plate",
-    description:
-      "(Contains G, MK, CY, E, S) Chicken Wings (Your Way), Crispy Potato Skins, Chilli Beef Nachos & Smoked Applewood Wheel Wings – Choose from Hot ‘n’ Spicy, Sticky Honey Glaze or BBQ Sauce",
+    description: "(Contains G, MK, CY, E, S) Chicken Wings (Your Way), Crispy Potato Skins, Chilli Beef Nachos & Smoked Applewood Wheel Wings – Choose from Hot ‘n’ Spicy, Sticky Honey Glaze or BBQ Sauce",
     price: "17.95",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0022_sharing%20platter.jpg?alt=media&token=867a3eae-2dec-4fd5-9d45-241a81e8855b",
     categories: ['foods', 'starters']
   },
   {
     name: "Herb-roasted Beef Tenderloin",
-    description:
-      "with Bordelaise sauce served with herb roasted new potatoes and a ragout of summer squash",
+    description: "with Bordelaise sauce served with herb roasted new potatoes and a ragout of summer squash",
     price: "17.95",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0021_herb%20roast%20beef.jpg?alt=media&token=2e6d7558-c006-421a-8e8a-9c06996784ef",
     categories: ['foods', 'mains']
   },
   {
     name: "Chicken Cordon Bleu",
-    description:
-      "with tasso ham and fontina cheese served with a potato gratin and mustard cream sauce",
+    description: "with tasso ham and fontina cheese served with a potato gratin and mustard cream sauce",
     price: "9.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0019_cordon%20bleu.jpg?alt=media&token=731ae8dd-8164-4692-9957-ea04ec87a6fb",
     categories: ['foods', 'mains']
   },
   {
     name: "Veal Parmesan",
     description: "with fettucine and marinara sauce",
     price: "12.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0018_veal%20parmesan.jpg?alt=media&token=bd17d9b1-2288-40fd-a337-9ae850886a43",
     categories: ['foods', 'mains']
   },
   {
     name: "Kansas City BBQ glazed Meatloaf",
     description: " with twice-baked potatoes and ratatouille",
     price: "14.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0014_meatloaf.jpg?alt=media&token=4c30cb70-2893-48aa-a701-a7fcd643f9fa",
     categories: ['foods', 'mains']
   },
   {
     name: "Pork Tenderloin",
-    description:
-      "stuffed with red pepper, spinach and goat cheese served with sweet potatoes and braised red cabbage",
+    description: "stuffed with red pepper, spinach and goat cheese served with sweet potatoes and braised red cabbage",
     price: "19.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0017_porktenderloin.jpg?alt=media&token=d7b107fd-2325-4bb2-b53d-4340e2588d45",
     categories: ['foods', 'mains']
   },
   {
     name: "Coq au Vin a la Bourguignonne",
-    description:
-      "Sauteed Chicken in a red wine sauce with bacon, mushrooms, pearl onions served with roasted garlic potato puree",
+    description: "Sauteed Chicken in a red wine sauce with bacon, mushrooms, pearl onions served with roasted garlic potato puree",
     price: "16.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0016_coq%20au%20vin.jpg?alt=media&token=a445f822-c2ea-4026-91cf-453f671ad9c6",
     categories: ['foods', 'mains']
   },
   {
     name: "Herb-basted Chicken Breast with Chasseur sauce",
     description: "served with glazed potatoes and a vegetable ragout",
     price: "15.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0015_herb%20basted%20chicken%20breast.jpg?alt=media&token=abdfc03a-73a6-430e-8721-6ea580353c7d",
     categories: ['foods', 'mains']
   },
   {
     name: "Curried Chicken",
-    description:
-      "with sauteed onions and diced tomatoes served with coconut rice",
+    description: "with sauteed onions and diced tomatoes served with coconut rice",
     price: "15.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0012_curried%20chicken.jpg?alt=media&token=17b8609e-283f-4b40-a69c-d10f0719d9ad",
     categories: ['foods', 'mains']
   },
   {
     name: "Pan-seared Sea Scallops and Shrimp",
     description: "served with creamed corn and fresh tomatoes",
     price: "13.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0011_scallops.jpg?alt=media&token=1a20c473-82e9-4971-8a6a-bd24ebecfd55",
     categories: ['foods', 'mains']
   },
   {
     name: "Honey, soy-glazed Salmon Filet",
     description: " with creamed leeks and mushrooms",
     price: "18.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0013_salmon.jpg?alt=media&token=583def6b-9635-4a8c-9c4a-877028bf940e",
     categories: ['foods', 'mains']
   },
   {
     name: "Red Snapper Filet",
-    description:
-      "basted with herbs and a lemon cream sauce served with roasted potatoes and a summer squash gratin",
+    description:"basted with herbs and a lemon cream sauce served with roasted potatoes and a summer squash gratin",
     price: "17.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0007_Red%20Snapper.jpg?alt=media&token=a50f623a-39fc-4355-904a-b585732bffaa",
     categories: ['foods', 'mains']
   },
   {
     name: "Pad Thai with Chicken and Shrimp",
-    description:
-      "Pad Thai with Chicken and Shrimp -- served with egg rolls and sweet red pepper dipping sauce",
+    description: "Pad Thai with Chicken and Shrimp -- served with egg rolls and sweet red pepper dipping sauce",
     price: "12.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0010_pad%20thai.jpg?alt=media&token=e4535d64-0785-4b31-b607-594369eac3a1",
     categories: ['foods', 'mains']
   },
   {
     name: "Crawfish Etouffee",
     description: "with fragrant basmati rice",
     price: "20.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0009_crawfish.jpg?alt=media&token=8aed8710-2536-4cbf-99fe-5c2ae9a02daa",
     categories: ['foods', 'mains']
   },
   {
     name: "Spinach Vegetable Lasagna",
     description: "with marinara sauce and classic caesar salad",
     price: "8.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0008_lasagna.jpg?alt=media&token=d59ad7bb-83e6-42b8-8a8b-18dd9da0f82e",
     categories: ['foods', 'desserts']
   },
 
   {
     name: "Chocolate Fudge Cake",
-    description:
-      "With Belgian chocolate sauce, real dairy ice cream, salted caramel crunch pearls and chocolate fudge pieces",
+    description:"With Belgian chocolate sauce, real dairy ice cream, salted caramel crunch pearls and chocolate fudge pieces",
     price: "12.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0006_chocolate%20fudge%20cake.jpg?alt=media&token=85e7cee9-cc21-4e8e-9857-5dfe85ddce8f",
     categories: ['foods', 'desserts']
   },
   {
     name: "Cherry Pie",
-    description:
-      "Dark sweet cherry pie, dusted with demerara sugar, served hot with your choice of real dairy ice cream, custard or fluffy cream",
+    description:"Dark sweet cherry pie, dusted with demerara sugar, served hot with your choice of real dairy ice cream, custard or fluffy cream",
     price: "6.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0004_cherry%20pie.jpg?alt=media&token=cdb98ddf-a62e-4d65-ae9b-99e5e58b1290",
     categories: ['foods', 'desserts']
   },
   {
     name: "Caramel Biscuit Cheesecake",
-    description:
-      "Baked New York style cheesecake with a buttery biscuit base, served with toffee fudge sauce, chocolate fudge pieces and fluffy cream",
+    description:"Baked New York style cheesecake with a buttery biscuit base, served with toffee fudge sauce, chocolate fudge pieces and fluffy cream",
     price: "18.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0005_caramel%20cheesecake.jpg?alt=media&token=ec3266fd-e71d-4716-a2a7-4c269177359c",
     categories: ['foods', 'desserts']
   },
   {
     name: "Lemon Meringue Pie",
-    description:
-      "With raspberries in sauce, real dairy ice cream and a white chocolate and raspberry shard",
+    description: "With raspberries in sauce, real dairy ice cream and a white chocolate and raspberry shard",
     price: "16.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0002_lemon%20merigue.jpg?alt=media&token=5eb4dff4-1f9c-4e83-bb13-0332109e613f",
     categories: ['foods', 'desserts']
   },
   {
     name: "Coconut and Black Cherry Rice Pudding",
-    description:
-      "Slowly cooked in coconut milk, served with raspberries in sauce",
+    description:"Slowly cooked in coconut milk, served with raspberries in sauce",
     price: "9.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0004_cherry%20pie.jpg?alt=media&token=cdb98ddf-a62e-4d65-ae9b-99e5e58b1290",
     categories: ['foods', 'desserts']
   },
   {
     name: "Treacle Sponge",
-    description:
-      "A fluffy sponge with lots of sticky treacle, and your choice of real dairy ice cream, custard or fluffy cream",
+    description:"A fluffy sponge with lots of sticky treacle, and your choice of real dairy ice cream, custard or fluffy cream",
     price: "7.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0001_treacle%20sponge%20pudding.jpg?alt=media&token=b94c4189-fafd-4b55-bad3-bf2ad5f6e724",
     categories: ['foods', 'desserts']
   },
   {
     name: "Belgian Chocolate Brownie",
-    description:
-      "The perfect brownie, with a raspberry & white chocolate shard and real dairy ice cream",
+    description: "The perfect brownie, with a raspberry & white chocolate shard and real dairy ice cream",
     price: "7.00",
-    photo: "",
+    photo: "https://firebasestorage.googleapis.com/v0/b/dumbwaiter-69a9a.appspot.com/o/food__0000s_0000s_0000_Belgian%20Chocolate%20Pudding.jpg?alt=media&token=1bf2b99c-fbf7-452c-abce-c631b48d9acf",
     categories: ['foods', 'desserts']
   }
 ];
