99cdd2913cbe5fe53e379e09a9d22f066b43bc47
Marlon Becker Santos <marlon@MBP-de-Marlon.lan>
2018-06-27 09:36:16 +0200

    first working querys with relations

--- api/models/Photo/Phototype.js
+++ api/models/Photo/Phototype.js
@@ -4,14 +4,13 @@ const {
   GraphQLString,
 } = require('graphql');
 
-
 const PhotoType = new GraphQLObjectType({
   name: 'Photo',
   description: 'This includes pictures of all stock',
   fields: () => ({
     url: {
-      type: GraphQLInt,
-      resolve: (photo) => photo.type,
+      type: GraphQLString,
+      resolve: (photo) => photo.url,
     },
     type: {
       type: GraphQLString,
