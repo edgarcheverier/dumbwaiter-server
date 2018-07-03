386f2bce03879767c32bf30b36f316766d639e5f
Marlon Becker Santos <marlon@MBP-de-Marlon.lan>
2018-07-03 18:19:25 +0200

    add restaurant description

--- api/models/Restaurant/RestaurantType.js
+++ api/models/Restaurant/RestaurantType.js
@@ -18,61 +18,64 @@ const RestaurantType = new GraphQLObjectType({
   fields: () => ({
     id: {
       type: GraphQLInt,
-      resolve: (restaurant) => restaurant.id,
+      resolve: restaurant => restaurant.id,
     },
     name: {
       type: GraphQLString,
-      resolve: (restaurant) => restaurant.name,
+      resolve: restaurant => restaurant.name,
     },
     description: {
       type: GraphQLString,
-      resolve: (restaurant) => restaurant.name,
+      resolve: restaurant => restaurant.description,
     },
     photos: {
       type: GraphQLList(PhotoType),
-      resolve: async (restaurant) => {
+      resolve: async restaurant => {
         const listPhotos = [];
-        return await Photo.findAll({ where: {
-          externalId: restaurant.id, type: 'RESTAURANT' }
+        return await Photo.findAll({
+          where: {
+            externalId: restaurant.id,
+            type: 'RESTAURANT',
+          },
         }).then(res => {
-          res.forEach((resultSetItem) => {
+          res.forEach(resultSetItem => {
             const photo = resultSetItem.get();
             listPhotos.push({ url: photo.url });
           });
           return listPhotos;
         });
-      }
+      },
     },
     type: {
       type: GraphQLString,
-      resolve: (restaurant) => restaurant.type,
+      resolve: restaurant => restaurant.type,
     },
     longitude: {
       type: GraphQLString,
-      resolve: (restaurant) => restaurant.longitude,
+      resolve: restaurant => restaurant.longitude,
     },
     latitude: {
       type: GraphQLString,
-      resolve: (restaurant) => restaurant.latitude,
+      resolve: restaurant => restaurant.latitude,
     },
     tables: {
       type: GraphQLList(TableType),
-      resolve (restaurant) {
+      resolve(restaurant) {
         return restaurant.getTables();
-      }
+      },
     },
     owner: {
       type: UserType,
-      resolve (restaurant) {
+      resolve(restaurant) {
         return restaurant.getUser();
-      }
+      },
     },
     products: {
       type: GraphQLList(ProductType),
-      resolve (restaurant) {
+      resolve(restaurant) {
         return restaurant.getProducts();
-      }
-    }
+      },
+    },
   }),
 });
 
