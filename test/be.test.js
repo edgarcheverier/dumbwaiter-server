7e99472910f33f79d70a69be933cd50ece5ec883
Amber Williams <Amber-Williams@users.noreply.github.com>
2019-03-10 19:29:45 +0100

    Testing: Intial testing enviornment set up for BE

---
+++ test/be.test.js
@@ -0,0 +1,11 @@
+import AuthController from './../api/controllers/Auth/AuthController';
+const express = require('express');
+//if you don't change package.json and update files imports will throw errors
+
+
+test('AuthController', () => {
+  expect(3).toBe(3);
+
+});
+
+//npx jest
